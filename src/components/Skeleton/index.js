3253edb3d3098c7a9ca8e9f4d7df58090c45c267
oliveirajoabe <oliveira_joabe@hotmail.com>
2021-05-06 15:03:15 -0300

    adição de loadgin skeleton

---
+++ src/components/Skeleton/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+function ComponentSkeleton() {
+    return (
+        <div className="skeleton-container">
+            <div className="skeleton-title">
+                <Skeleton height={28} width={300} />
+            </div>
+
+            <ul>
+                {Array(6)
+                .fill()
+                .map((item, index) => (
+                    <li key={index}>
+                    <div className="item-group">
+                        <div className="skeleton-item">
+                        <Skeleton height={20} width={`100%`} />
+                        </div>
+
+                        <div className="skeleton-item">
+                        <Skeleton height={20} width={`100%`} />
+                        </div>
+                    </div>
+
+                    <div className="item-group">
+                        <div className="skeleton-item">
+                        <Skeleton height={20} width={`100%`} />
+                        </div>
+
+                        <div className="skeleton-item">
+                        <Skeleton height={20} width={`100%`} />
+                        </div>
+                    </div>
+
+                    <div className="item-group">
+                        <div className="skeleton-item">
+                        <Skeleton height={20} width={`100%`} />
+                        </div>
+
+                        <div className="skeleton-item">
+                        <Skeleton height={20} width={`100%`} />
+                        </div>
+                    </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ComponentSkeleton;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,31 +4,35 @@ import {FcNext,FcPrevious} from 'react-icons/fc';
 import { api } from '../services/api';
 import styles from './home.module.scss';
 
+import Skeleton from '../components/Skeleton';
 
 export default function Home() {
   const [pokemons, setPokemons] = useState([]);
   const [pageAtual, setPageAtual] = useState(0);
   const [pokemonStatus, setPokemonStatus] = useState([]);
   const [pokemonId, setPokemonId] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
-    async function loadPokemon() {
-      const {data} = await api.get("/pokemon", {
-        params: {
-          offset:pageAtual,
-          limit:12
-        }
-      });
-      setPokemons(data.results);
-    };
+    setTimeout(()=>{
+      async function loadPokemon() {
+        const {data} = await api.get("/pokemon", {
+          params: {
+            offset:pageAtual,
+            limit:12
+          }
+        });
+        setPokemons(data.results);
+      };
+      setLoading(false);
+      loadPokemon();
+    }, 3000)
+    
     async function uniquePokemon() {
       const { data } = await api.get(`/pokemon/${pokemonId}`);
       setPokemonStatus(data.stats);
     };
-
     uniquePokemon();
-    loadPokemon();
-
   }, [pageAtual, pokemonId]);
   
   const id = pokemons.map((item)=>{
@@ -39,33 +43,36 @@ export default function Home() {
 
   return (
     <>
-      <div className={styles.flipCard}>    
-        {pokemons.map((item, index) => {
-          return (
-            <div className={styles.flipCardInner} key={index} onMouseOver={()=>setPokemonId(id[index])}>
-              <div className={styles.flipCardFront}>
-                <img width="80" src={`https://pokeres.bastionbot.org/images/pokemon/${id[index]}.png`}/>
-                <h2>{item.name}</h2>
+      {loading && <Skeleton/>}
+      {!loading &&
+        <div className={styles.flipCard}>    
+          {pokemons.map((item, index) => {
+            return (
+              <div className={styles.flipCardInner} key={index} onMouseOver={()=>setPokemonId(id[index])}>
+                <div className={styles.flipCardFront}>
+                  <img width="80" src={`https://pokeres.bastionbot.org/images/pokemon/${id[index]}.png`}/>
+                  <h2>{item.name}</h2>
+                </div>
+                <div className={styles.flipCardBack} >
+                  {pokemonStatus.map((item, index) => (
+                    <div className={styles.status} key={index}>
+                      <p>{item.stat.name}</p>
+                      <p>{item.base_stat}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
-              <div className={styles.flipCardBack} >
-                {pokemonStatus.map((item, index) => (
-                  <div className={styles.status} key={index}>
-                    <p>{item.stat.name}</p>
-                    <p>{item.base_stat}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )
-        })}
-      </div>
+            )
+          })}
+        </div>
+      }
 
       
       <div className={styles.pagination}>
         <div className={styles.paginationArea}>
-            <div className={styles.page} onClick={()=>setPageAtual(pageAtual-12)} disabled><FcPrevious/></div>
+            <div className={styles.page} onClick={()=>{setLoading(true); setPageAtual(pageAtual-12)}} disabled><FcPrevious/></div>
           
-          <div className={styles.page} onClick={()=>setPageAtual(pageAtual+12)}><FcNext/></div>
+          <div className={styles.page} onClick={()=>{setLoading(true); setPageAtual(pageAtual+12)}}><FcNext/></div>
         </div>
       </div>
     </>
